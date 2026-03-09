10e565670170e50ce4b94f9979a0d3c3960c997e
Martin Schulze <martin@slash-m.com>
2026-03-09 20:33:26 +0100

    fix: address review findings for server-recents-pins

--- app/routes/home/home.route.tsx
+++ app/routes/home/home.route.tsx
@@ -18,11 +18,7 @@ import { Section } from "~/components/Container";
 import { TreeNode } from "~/components/DirectoryView/buildDirectoryTree";
 import { DirectoryView } from "~/components/DirectoryView/DirectoryView";
 import { useInitConnections } from "~/hooks/useInitConnections";
-import {
-  loadConnectionNodes,
-  type SerializedPinnedPath,
-  type SerializedRecentlyViewed,
-} from "~/routes/connections/loadConnectionNodes";
+import { loadConnectionNodes } from "~/routes/connections/loadConnectionNodes";
 import { deleteConnectionConfig } from "~/utils/connectionConfig.server";
 import { getFileType, IMAGE_FILE_TYPES } from "~/utils/fileType";
 
