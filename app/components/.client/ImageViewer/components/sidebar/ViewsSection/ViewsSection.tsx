dc8b8d6a66a08f4854f944b7fde0ee816b5c85ec
Johannes Siedler <johannes@cytario.com>
2026-09-14 14:09:36 +0200

    Revert "fix: restore 3-group section layout in ViewsSection"
    This reverts commit 13af5aa48bf9b0c2a8379e8fe31f2a7b135d710d.

--- app/components/.client/ImageViewer/components/sidebar/ViewsSection/ViewsSection.tsx
+++ app/components/.client/ImageViewer/components/sidebar/ViewsSection/ViewsSection.tsx
@@ -6,7 +6,6 @@ import { ViewRadioButton } from "./ViewRadioButton";
 import type { ViewKey } from "./ViewStateIcon";
 import { useViewerStore } from "../../../state/store/core/ViewerStoreContext";
 import { select } from "../../../state/store/selectors";
-import { Divider } from "~/components/Divider/Divider";
 import { Section } from "~/components/Section/Section";
 
 /** Sidebar views control: split-panel layout (add/remove panels, assign VCS). */
@@ -25,28 +24,6 @@ export function ViewsSection() {
     return "local";
   }
 
-  const localViews = layersStates
-    .map((ls, index) => ({ ls, index }))
-    .filter(({ ls }) => ls.author === currentUserId && !ls.shared);
-
-  const ownSharedViews = layersStates
-    .map((ls, index) => ({ ls, index }))
-    .filter(({ ls }) => ls.author === currentUserId && ls.shared);
-
-  const peerSharedViews = layersStates
-    .map((ls, index) => ({ ls, index }))
-    .filter(({ ls }) => ls.author !== currentUserId);
-
-  const renderItem = ({ index }: { index: number }) => (
-    <ViewRadioButton
-      key={index}
-      index={index}
-      canDelete={layersStates.length > 1}
-      viewState={viewStateFor(index)}
-      onDelete={() => removeChannelsState(index)}
-    />
-  );
-
   return (
     <Section
       pillar="views"
@@ -61,26 +38,17 @@ export function ViewsSection() {
         aria-label="Views"
         value={String(activePresetIndex)}
         onChange={(value) => setActivePresetIndex(Number(value))}
-        className="flex flex-col gap-4 px-3 pt-2 pb-4 border-b border-border shrink-0"
+        className="flex flex-col gap-1 px-3 pt-2 pb-4 border-b border-border shrink-0"
       >
-        {localViews.length > 0 && (
-          <section className="flex flex-col gap-2">
-            <Divider>My views</Divider>
-            {localViews.map(renderItem)}
-          </section>
-        )}
-        {ownSharedViews.length > 0 && (
-          <section className="flex flex-col gap-2">
-            <Divider>Shared by me</Divider>
-            {ownSharedViews.map(renderItem)}
-          </section>
-        )}
-        {peerSharedViews.length > 0 && (
-          <section className="flex flex-col gap-2">
-            <Divider>Shared with me</Divider>
-            {peerSharedViews.map(renderItem)}
-          </section>
-        )}
+        {layersStates.map((layersState, index) => (
+          <ViewRadioButton
+            key={index}
+            index={index}
+            canDelete={layersStates.length > 1}
+            viewState={viewStateFor(index)}
+            onDelete={() => removeChannelsState(index)}
+          />
+        ))}
       </RadioGroup>
     </Section>
   );
